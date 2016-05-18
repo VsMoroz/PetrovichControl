198547e63bf619b58519e311cf71a34d9328e5bb
AIakovenko <aleks.iakovenko@gmail.com>
2016-05-18 10:27:46 +0300

    Created a project and plugged in a Spring MVC

---
+++ BackEnd/ml.locator/src/main/java/ml/locator/service/EmailService.java
@@ -0,0 +1,30 @@
+package ml.locator.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ml.locator.model.User;
+
+@RestController
+@RequestMapping("email")
+public class EmailService {
+	@Autowired
+	User user;
+	@RequestMapping(value="sendto", method=RequestMethod.GET)
+    public String sendTo(@RequestParam String email, @RequestParam String message){
+		
+		return "{email:" + email+ ", message:" + message + "}";
+		
+	}
+	
+	@RequestMapping(value="test", method=RequestMethod.GET)
+    public User test(@RequestParam String test){
+		return user;
+	}
+	
+
+}
